661b33d8b11d903e7b9d3927bc4c11fe5ca1b910
Kitsana Rattanapholsaen <kitsana971@gmail.com>
2021-06-25 02:11:40 +0700

    type cast

---
+++ Typecast.java
@@ -0,0 +1,14 @@
+class typecast {
+    public static void main(String[] args) {
+        //แปลงค่า เล็ก -> ใหญ่
+        int numInt = 10;
+        double numDouble = numInt;
+
+        //แปลงค่าจาก ใหญ่ -> เล็ก
+        double numDouble = 10.0;
+        int numInt=(int)numDouble; //ต้องมี (int) อยู่ข้างหน้า
+
+        System.out.println(numInt);
+        System.out.println(numDouble);
+    }
+}
--- Variable.java
+++ Variable.java
@@ -1,9 +1,14 @@
-public class Variable {
+class Variable {
     public static void main(String[] args) {
         int num1= 10;//ค่าเริ่มต้น 10 , 20
-        int num2=20;
+        final int num2=20; //ใส่ final ทำให้เป็นค่าคงที่ ไม่สามารถแก้ไขได้
         num1=50; //เปลี่ยนค่า num 1
         System.out.println("แสดงค่าที่ 1 = "+num1); //นำตัวอักษรมา + กับตัวแปร num
         System.out.println("แสดงค่าที่ 2 = "+num2);
+        /////////////////////////////////////////
+        System.out.println(Integer.MAX_VALUE); //ทดสอบการเก็บค่าสูงสุด
+        System.out.println(Integer.MIN_VALUE); //ทดสอบการเก็บต่ำสูงสุด
+        /////////////////////////////////////////
+
     }
 }
