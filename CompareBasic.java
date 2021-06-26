8b154ffe9d48b56e8511a57753bed24994d6ec09
Kitsana Rattanapholsaen <kitsana971@gmail.com>
2021-06-26 21:44:36 +0700

    compare

---
+++ CompareBasic.java
@@ -0,0 +1,10 @@
+public class CompareBasic {
+    public static void main(String[] args) {
+        //Ture False
+        int a=10 , b=20;
+       
+        System.out.println(a+ " มีค่าเท่ากับ " +b+" จริงหรือไม่ = "+(a==b));
+        System.out.println(a+ " ไม่เท่ากับ " +b+" จริงหรือไม่ = "+(a!=b));
+
+    }
+}
