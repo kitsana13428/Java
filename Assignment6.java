200337d1c45f01966812b2448c6cc310b394d8d6
Kitsana Rattanapholsaen <kitsana971@gmail.com>
2021-06-30 00:26:53 +0700

    Fahren to cel
    อุณภูมิ

---
+++ Assignment6.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Assignment6 {
+    public static void main(String[] args) {
+        Scanner kb=new Scanner(System.in);
+        System.out.print("ป้อนอุณภูมิ (ฟาเรนไฮน์) = "); //กรอกฟาเรนไฮน์
+        float fahren=kb.nextFloat();
+        float cel = (fahren-32)*5/9; //คำนวณไฟเซลเซียส
+        System.out.println(fahren+" องศาฟาเรนไฮร์ = "+ cel +" องศาเซลเซียส");
+    }
+}
