21731b3f22a133fa3a6c08d7dd5ceab7971f36c2
Kitsana Rattanapholsaen <kitsana971@gmail.com>
2021-06-29 21:51:21 +0700

    Assignment2

---
+++ Assignment2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Assignment2{
+    public static void main(String[] args) {
+        Scanner kb=new Scanner(System.in);
+
+        //ป้อนค่าเลขตัวที่ 1
+        System.out.print("ป้อนเลขตัวที่ 1 : ");
+        int number1=kb.nextInt(); 
+        
+        //ป้อนตัวที่ 2
+        System.out.print("ป้อนตัวเลขที่ 2 : ");
+        int number2=kb.nextInt();
+
+        if(number1>number2){
+            System.out.println(number1+" มากกว่า "+number2);
+        }else if(number1<number2){
+            System.out.println(number1+" น้อยกว่า "+number2);
+        }else{
+            System.out.println(number1+ " เท่ากับ "+number2);
+        }
+    }
+}
