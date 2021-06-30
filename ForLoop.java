bebca45e7f2578d38ea592014be88a0e79a30100
Kitsana Rattanapholsaen <kitsana971@gmail.com>
2021-06-30 15:06:55 +0700

    For loop

---
+++ ForLoop.java
@@ -0,0 +1,8 @@
+public class ForLoop {
+    public static void main(String[] args) {
+
+        for(int count=1;count<=10;count++){//ถ้าไม่เท่ากับ 10 ให้วนลูป
+            System.out.println("Hello World");
+        }
+    }
+}
