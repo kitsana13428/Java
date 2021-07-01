91c982fcd9e89f01c4bb37d8a2058728f3c106e9
Kitsana Rattanapholsaen <kitsana971@gmail.com>
2021-07-01 21:22:43 +0700

    Array ForEach

---
+++ Arrayforeach.java
@@ -0,0 +1,11 @@
+public class Arrayforeach {
+    public static void main(String[] args) {
+        String [] color={"red","yellow","black","blue"};
+        //loop ดึงข้อมูลออกมา
+        System.out.println("-----------");
+        for (String data : color){ 
+            System.out.println(data);
+        }
+
+    }
+}
