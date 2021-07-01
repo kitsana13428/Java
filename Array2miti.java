4291880f2df733028b85bb5904b8d32082bd7fc8
Kitsana Rattanapholsaen <kitsana971@gmail.com>
2021-07-01 21:38:22 +0700

    Array 2 miti

---
+++ Array2miti.java
@@ -0,0 +1,12 @@
+public class Array2miti {
+    public static void main(String[] args) {
+        //Array 2 มิติ
+        String [] [] product = {
+            {"ส้ม", "กล้วย", "องุ่น"},
+            {"พัดลม", "ทีวี", "ตู้เย็น"},
+            {"ปากกา", "ไม้บรรทัด", "สมุด"}
+        };
+        //product[เลขแถว][เลขคอลัมน์]
+        System.out.println(product[0][1]);
+    }
+}
