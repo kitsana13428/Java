da8cd25c47ac1d81579e2f708bb1e13de185383c
Kitsana Rattanapholsaen <kitsana971@gmail.com>
2021-07-01 13:55:09 +0700

    Array 1

---
+++ Array1.java
@@ -0,0 +1,16 @@
+public class Array1 {
+    public static void main(String[] args) {
+        //แบบกำหนดขนาดไว้
+        int[] number=new int[4];
+        //กำหนดค่าลง Array
+        number[0]=1;
+        number[1]=2;
+        number[2]=3;
+        number[3]=4;
+        System.out.println(number[2]);
+
+        //แบบกำหนดสมาชิกแล้ว
+        String []color={"red", "yellow", "blue"};
+        System.out.println(color[1]);
+    }
+}
