13a7a8184e7cb5dca67ae06d8d0055fa2ada9b37
Kitsana Rattanapholsaen <kitsana971@gmail.com>
2021-07-05 15:14:31 +0700

    Buffer Reader

--- FileDemo2.java
+++ FileDemo2.java
@@ -3,9 +3,11 @@ public class FileDemo2 {
     public static void main(String[] args) {
         try {
             FileReader reader=new FileReader(new File("test.txt")); //อ่านไฟล์
-            int data;
-            while((data = reader.read()) !=-1){ //ดึงตัวอักษรทุกตัว (ไม่น้อยกว่า -1) เก็บไว้ในตัวแปร data
-                System.out.printf("%c",data); //ดึงข้อมูลตัวอักษรออกมา
+            BufferedReader br = new BufferedReader(reader);
+            String message = "";
+
+            while((message = br.readLine()) !=null){ //ลูปข้อมูลทีละบรรทัด
+                System.out.println(message); //แสดงผลทีละบรรทัด
             }
         } catch (Exception e) {
             e.printStackTrace();
