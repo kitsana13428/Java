b1a82bc7cf9aca5e9666ded3a93151a5d40e8cd5
Kitsana Rattanapholsaen <kitsana971@gmail.com>
2021-07-05 13:01:15 +0700

    String +

--- StringMethod.java
+++ StringMethod.java
@@ -10,5 +10,11 @@ public class StringMethod {
         }else{ //ถ้าไม่ใช่ = หญิง
             System.out.println("เพศหญิง");
         }
+        
+        //String => Array ใช้สัญลักษณ์
+        String data = "มะม่วง,กล้วย,ขนุน,ลำไย";
+        String [] fruit = data.split(","); //ใช้ , แบ่งให้เป็น Array
+
+        System.out.println(fruit[1]);
     }
 }
